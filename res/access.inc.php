98b18a9b0e3c90293bffeb83409234f32a90616a
contratop <franciscojaviermedina12@hotmail.com>
2019-04-25 04:26:21 +0200

    WEBPAGE PROVISSIONAL INDEV 0.1

---
+++ res/access.inc.php
@@ -0,0 +1,23 @@
+<?php
+
+// Users data
+$imSettings['access']['webregistrations_gid'] = 'oy78iqrt';
+$imSettings['access']['users'] = array(
+	'msicojonesalviento@huevos.com' => array(
+		'groups' => array('nizq4qz3'),
+		'id' => '6slvi3cs',
+		'firstname' => 'Admin',
+		'lastname' => '',
+		'password' => 'y7pvke3t',
+		'email' => 'msicojonesalviento@huevos.com',
+		'page' => false
+	)
+);
+
+// Admins list
+$imSettings['access']['admins'] = array('6slvi3cs');
+
+// Page/Users permissions
+$imSettings['access']['pages'] = array();
+
+// End of file access.inc.php
---
+++ res/search.inc.php
@@ -0,0 +1,39 @@
+<?php
+
+/*****************
+	GENERAL SETTINGS
+*****************/
+$imSettings['search']['general'] = array(
+	'menu_position' => 'left',
+	'defaultScope' => array(
+		'0' => 'index.html',
+		'3' => 'instalar-enso.html',
+		'4' => 'vita-gamejam-2019.html',
+		'5' => 'estado-enso-online.html'
+	),
+	'extendedScope' => array(
+	)
+);
+
+/*****************
+	PRODUCTS SEARCH
+*****************/
+$imSettings['search']['products'] = array(
+);
+
+/*****************
+	IMAGES SEARCH
+*****************/
+$imSettings['search']['images'] = array(
+);
+
+/*****************
+	VIDEOS SEARCH
+*****************/
+$imSettings['search']['videos'] = array(
+);
+$imSettings['search']['dynamicobjects'] = array(
+
+);
+
+// End of file search.inc.php
